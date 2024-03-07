e3c70e565f964f0ca8422da1a7f9b7f937fe1fa7
Ahmed Allam <ahmedmohamedallam230@gmail.com>
2024-03-07 20:06:15 +0200

    upadated

--- Tranier System/Controllers/InstructorController.cs
+++ Tranier System/Controllers/InstructorController.cs
@@ -11,7 +11,7 @@ namespace Tranier_System.Controllers
         //{
         //    count = 0;
         //}
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {
             //count++;
             //HttpContext.Session.SetString("","");
@@ -20,8 +20,18 @@ namespace Tranier_System.Controllers
             //options.Expires = DateTime.Now.AddDays(2);
             //Response.Cookies.Append("", "",options);
             //var instructorsModel = db.Instructor.Select(i=> new {i.Id,i.Name,i.Image,i.Salary,i.Address,Department = i.Department.Name,Course = i.Course.Name }).ToList();
-            List<Instructor> instructors = db.Instructor.ToList();
-            
+            int content = 3;
+            int skip = (page - 1) * content;
+            List<Instructor> instructors = db.Instructor.Skip(skip).Take(content).ToList();
+            int totalInstructors = db.Instructor.Count();
+            ViewData["Page"] = page;
+            ViewData["content"] = content;
+            ViewData["TotalItems"] = totalInstructors;
+            //List<Course> Course = db.course.ToList();
+            //ViewData["Deps"] = db.Department.ToList();
+            //return View("Index", courses);
+
+            //List<Instructor> instructors = db.Instructor.ToList();
             return View("Index",instructors);
         }
         public IActionResult Details(int id)
@@ -76,6 +86,46 @@ namespace Tranier_System.Controllers
         {
             List<Course> crs = db.course.Where(c=>c.DepartmentId==id).ToList();
             return Json(crs);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewData["Deps"] = db.Department.ToList();
+            Course data = db.course.SingleOrDefault(c => c.Id == id);
+            return View("Edit", data);
+        }
+        [HttpPost]
+        public IActionResult Edit(Instructor ins, IFormFile Image)
+        {
+            if (ins.Name != null && ins.Address != null && ins.Salary != null && ins.DepartmentId != null && ins.CourseId != null)
+            {
+                if (Image.Length > 0)
+                {
+                    string FileName = Guid.NewGuid().ToString() + "_" + Path.GetFileName(Image.FileName);
+                    string path = $"wwwroot/images/{FileName}";
+                    FileStream fs = new FileStream(path, FileMode.Create);
+                    Image.CopyTo(fs);
+                    ins.Image = FileName;
+                }
+                db.Update(ins);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            ViewData["Deps"] = db.Department.ToList();
+            return PartialView("_DataPartial",ins);
+        }
+        public IActionResult DataPartial(int id)
+        {
+            Instructor instructor = db.Instructor.SingleOrDefault(i => i.Id == id);
+            return PartialView("_DataPartial", instructor);
+        }
+        public IActionResult DataEditPartial(int id)
+        {
+            ViewData["Deps"] = db.Department.ToList();
+            ViewData["Crs"] = db.course.ToList();
+            Instructor instructor = db.Instructor.SingleOrDefault(i => i.Id == id);
+            return PartialView("_DataEditPartial", instructor);
         }
     }
 }
